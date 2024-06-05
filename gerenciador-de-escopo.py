843f3ca088fdd3ac659c5ed32e6394e12db817a3
Joao Pedro <jpcsnoberto@gmail.com>
2024-06-05 00:30:00 -0300

    código comentado 📝

--- gerenciador-de-escopo.py
+++ gerenciador-de-escopo.py
@@ -1,14 +1,17 @@
+# Declara variáveis no escopo atual
 def declarar_variaveis (linha, escopos):
     partes = linha.split()
-    tipo = partes[0]
-    declaracoes = " ".join(partes[1:]).split(",")
+    tipo = partes[0] # A primeira posição do array de partes contém o tipo da variável (NUMERO ou CADEIA)
+    declaracoes = " ".join(partes[1:]).split(",") # Separa as declarações individuais de variáveis (a virgula é o separador)
 
     for declaracao in declaracoes:
         if "=" in declaracao:
+            # Se a declaração possuir atribuição, separa o nome e o valor
             nome, valor = declaracao.split("=")
             nome = nome.strip()
             valor = valor.strip()
 
+            # Cast do valor para o tipo correto
             if tipo == "NUMERO":
                 if ("." in valor):
                     valor = float(valor)
@@ -17,25 +20,31 @@ def declarar_variaveis (linha, escopos):
             elif tipo == "CADEIA":
                 valor = valor.strip()
         else:
+            # Se não possuir atribuição, declara a variável com valor padrão
             nome = declaracao.strip()
             if (tipo == "NUMERO"):
                 valor = 0
             else:
                 valor = '""'
         
+        # Adiciona a variável ao escopo atual
         escopos[-1][nome] = {"tipo": tipo, "valor": valor}
 
+# Atribuição de valor a uma variável já declarada
 def atribuicao(linha, escopos, num_linha):
     nome, valor = linha.split("=")
     nome = nome.strip()
     valor = valor.strip()
     tipo = None
 
+    # Procura a variável a partir do escopo atual
     variavel_original = procurar_variavel(escopos, nome)
     if variavel_original:
-        if valor[0].islower():
+        if valor[0].islower(): # Se o valor for uma variável (começar com letra minúscula)
+            # Se a variável de referência for encontrada, atribui o valor dela à variável original
             variavel_referencia = procurar_variavel(escopos, valor)
             if variavel_referencia:
+                # Se os tipos forem compatíveis, atribui o valor
                 if variavel_referencia["tipo"] == variavel_original["tipo"]:
                     if nome in escopos[-1]:
                         escopos[-1][nome]["valor"] = variavel_referencia["valor"]
@@ -46,12 +55,15 @@ def atribuicao(linha, escopos, num_linha):
             else:
                 print(f"Erro linha {num_linha} - variável não declarada")
         else:
+            # Tenta fazer cast do valor para descobrir o tipo
+            # Utilizei try/except pois o Python gera erro e para a execução se o cast não for possível
             try:
                 float(valor)
                 tipo = "NUMERO"
             except ValueError:
                 tipo = "CADEIA"
 
+            # Se os tipos forem compatíveis, atribui o valor
             if tipo == variavel_original["tipo"]:
                     if nome in escopos[-1]:
                         escopos[-1][nome]["valor"] = valor
@@ -62,12 +74,14 @@ def atribuicao(linha, escopos, num_linha):
     else:
         print(f"Erro linha {num_linha} - variável não declarada")
 
+# Procura uma variável na pilha de escopos (a partir do topo)
 def procurar_variavel(escopos, nome):
     for escopo in reversed(escopos):
         if nome in escopo:
             return escopo[nome]
     return None
 
+# Imprime o valor de uma variável
 def imprimir_variavel(linha, escopos, num_linha):
     nome = linha.split()[1].strip()
     variavel = procurar_variavel(escopos, nome)
@@ -84,13 +98,14 @@ def main():
     escopos = []
     linhas = codigo.strip().split("\n")
     
+    # Itera sobre as linhas do código realizando as operações necessárias
     for num_linha, linha in enumerate(linhas, 1):
         linha = linha.strip()
         if linha.startswith("BLOCO"):
-            escopos.append({})
+            escopos.append({}) # Adiciona um novo escopo
         elif linha.startswith("FIM"):
             if (escopos):
-                escopos.pop()
+                escopos.pop() # Fecha o escopo atual
             else:
                 print(f"Erro linha {num_linha} - nenhum escopo para fechar")
         elif linha.startswith("NUMERO") or linha.startswith("CADEIA"):
